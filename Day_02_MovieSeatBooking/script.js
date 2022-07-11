46d50deee4cfa6114bb9dc414fb14fb1199427d8
TaylorDickens <justindickens17@gmail.com>
2022-07-11 16:00:01 -0400

    populated our local storage to the UI thus on reload, our information is still saved to our screen.

--- Day_02_MovieSeatBooking/script.js
+++ Day_02_MovieSeatBooking/script.js
@@ -4,6 +4,8 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
+populateUI();
+
 let ticketPrice = parseInt(movieSelect.value);
 
 // Save selected movie index and price
@@ -30,6 +32,25 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from localstorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 // Movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = parseInt(e.target.value);
@@ -50,3 +71,6 @@ container.addEventListener("click", (e) => {
     updateSelectedCount();
   }
 });
+
+// Initial count and total set
+updateSelectedCount();
