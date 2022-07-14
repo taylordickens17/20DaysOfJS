baedd0ff2742c4b4d739b67da8c75a89a64dc854
TaylorDickens <justindickens17@gmail.com>
2022-07-14 09:45:46 -0400

    updated the dom when we generate new users and display it to the DOM, format the currency to be actual money, and added a new user when clicking the new user button

--- Day_05_DOMArrayMethods/script.js
+++ Day_05_DOMArrayMethods/script.js
@@ -29,4 +29,29 @@ async function getRandomUser() {
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+  providedData.forEach((person) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${person.name}</strong> ${formatMoney(
+      person.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return `$${number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")}`;
+}
+
+// Event listeners
+addUserBtn.addEventListener("click", getRandomUser);
