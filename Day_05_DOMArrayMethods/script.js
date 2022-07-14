5767dcd12b818aec81e524e02de7f3f9075116a0
TaylorDickens <justindickens17@gmail.com>
2022-07-14 10:20:36 -0400

    created a function that calculates everyones wealth combined using the reduce method

--- Day_05_DOMArrayMethods/script.js
+++ Day_05_DOMArrayMethods/script.js
@@ -49,6 +49,18 @@ function showMillionaires() {
   updateDOM();
 }
 
+// Calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc = acc + user.money), 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+
+  main.appendChild(wealthEl);
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -81,3 +93,4 @@ addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
 showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
