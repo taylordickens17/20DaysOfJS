6ae5ce8ae88d9f29bf16e0cf9d7f5277ef41c7de
TaylorDickens <justindickens17@gmail.com>
2022-07-17 22:14:01 -0400

    created the random feature to generate a random meal, display the meal to the dom

--- Day_08_MealFinder/script.js
+++ Day_08_MealFinder/script.js
@@ -58,6 +58,21 @@ function getMealById(mealID) {
     });
 }
 
+// Fetch random meal from API
+function getRandomMeal() {
+  // Clear meals and heading
+  mealsEl.innerHTML = "";
+  resultHeading.innerHTML = "";
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
 // Add Meal to DOM
 function addMealToDOM(meal) {
   const ingredients = [];
@@ -93,6 +108,7 @@ function addMealToDOM(meal) {
 
 //   Event listeners
 submit.addEventListener("submit", searchMeal);
+random.addEventListener("click", getRandomMeal);
 
 // find method just finds a particular item that you pass it.
 mealsEl.addEventListener("click", (e) => {
