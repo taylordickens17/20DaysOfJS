a24f97d60a1acd6e31f31b65c508bdce528936dc
TaylorDickens <justindickens17@gmail.com>
2022-07-17 20:49:13 -0400

    added the show notification function to add and remove the show class that allows the notification to pop up. Added a keydown event listener that checks to see if you select a letter and is included in the selectedWord array

--- Day_07_HangmanGame/script.js
+++ Day_07_HangmanGame/script.js
@@ -11,7 +11,7 @@ const words = ["application", "programming", "interface", "wizard"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = ["w", "i", "z", "a", "r", "d"];
+const correctLetters = [];
 const wrongLetters = [];
 
 // Show the hidden word
@@ -37,5 +37,45 @@ function displayWord() {
     popup.style.display = "flex";
   }
 }
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+  console.log("Update wrong");
+}
+
+// Show notification
+function showNotification() {
+  notification.classList.add("show");
+
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+// Keydown letter press
+window.addEventListener("keydown", (e) => {
+  //   console.log(e.keyCode);
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
 
 displayWord();
