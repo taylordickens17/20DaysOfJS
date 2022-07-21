b0a3d91a6c18731df776f3836e6c1147c15956d8
TaylorDickens <justindickens17@gmail.com>
2022-07-21 10:33:37 -0400

    created the functionality to set the difficulty using local storage

--- Day_12_TypingGame/script.js
+++ Day_12_TypingGame/script.js
@@ -41,6 +41,18 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Set difficulty to value in ls or medium
+let difficulty =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
+// Set difficulty select value
+difficultySelect.value =
+  localStorage.getItem("difficulty") !== null
+    ? localStorage.getItem("difficulty")
+    : "medium";
+
 // Focus on text on start
 text.focus();
 
@@ -89,6 +101,7 @@ function gameOver() {
 
 addWordToDOM();
 
+// Typing
 text.addEventListener("input", (e) => {
   const insertedText = e.target.value;
 
@@ -99,8 +112,23 @@ text.addEventListener("input", (e) => {
     // Clear
     e.target.value = "";
 
-    time += 2;
+    if (difficulty === "hard") {
+      time += 1;
+    } else if (difficulty === "medium") {
+      time += 3;
+    } else {
+      time += 5;
+    }
 
     updateTime();
   }
+});
+
+// Settings btn click
+settingsBtn.addEventListener("click", () => settings.classList.toggle("hide"));
+
+// Settings select
+settingsForm.addEventListener("change", (e) => {
+  difficulty = e.target.value;
+  localStorage.setItem("difficulty", difficulty);
 });
