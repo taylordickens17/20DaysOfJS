0fec1514ceafa912b04ee1db9deb147767227def
TaylorDickens <justindickens17@gmail.com>
2022-07-24 22:47:41 -0400

    added pagination functionality to the app

--- Day_15_LyricsSearchApp/script.js
+++ Day_15_LyricsSearchApp/script.js
@@ -7,14 +7,72 @@ const apiURL = "https://api.lyrics.ovh";
 
 // Search by song or artist
 async function searchSongs(term) {
-  const fetchSongs = await fetch(`${apiURL}/suggest/${term}`);
-  const songData = await fetchSongs.json();
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
 
-  showData(songData);
+  showData(data);
 }
 
 // Show song and artist in DOM
-function showData(songData) {}
+function showData(data) {
+  //   let output = "";
+
+  //   data.data.forEach((song) => {
+  //     output += `
+  //             <li>
+  //                 <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  //                 <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+  //             </li>
+  //         `;
+  //   });
+
+  //   result.innerHTML = `
+  //     <ul class="songs">
+  //         ${output}
+  //     </ul>
+  //   `;
+
+  result.innerHTML = `
+    <ul class="songs">
+        ${data.data
+          .map(
+            (song) =>
+              `
+            <li>
+                <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+                <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+            </li>
+        `
+          )
+          .join("")}
+    </ul>
+  `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+        ${
+          data.prev
+            ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Previous</button>`
+            : ``
+        }
+        ${
+          data.next
+            ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+            : ``
+        }
+    `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+// Get previous and next songs
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
+}
 
 // Event listeners
 form.addEventListener("submit", (e) => {
