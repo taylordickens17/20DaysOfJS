29c87e93159b0fbc5b883a759a467ee4458c2aea
TaylorDickens <justindickens17@gmail.com>
2022-07-27 13:52:40 -0400

    gave the lose functionality to reset all the bricks and restart at 0

--- Day_17_BreakoutGame/script.js
+++ Day_17_BreakoutGame/script.js
@@ -138,9 +138,33 @@ function moveBall() {
         ) {
           ball.dy *= -1;
           brick.visible = false;
+
+          increaseScore();
         }
       }
     });
+  });
+
+  //   Hit bottom wall - Lose
+  if (ball.y + ball.size > canvas.height) {
+    showAllBricks();
+    score = 0;
+  }
+}
+
+// Increase Score
+function increaseScore() {
+  score++;
+
+  if (score % (brickRowCount * brickRowCount) === 0) {
+    showAllBricks();
+  }
+}
+
+// Make all bricks appear
+function showAllBricks() {
+  bricks.forEach((column) => {
+    column.forEach((brick) => (brick.visible = true));
   });
 }
 
