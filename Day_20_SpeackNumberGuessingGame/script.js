11a1c61981ef5b8be04e3f7ac6b81cc4f92a2577
TaylorDickens <justindickens17@gmail.com>
2022-07-29 09:42:41 -0400

    Finished my last day of 20 days of JS :)

--- Day_20_SpeackNumberGuessingGame/script.js
+++ Day_20_SpeackNumberGuessingGame/script.js
@@ -1,4 +1,4 @@
-const msg = document.getElementById("msg");
+const msgEl = document.getElementById("msg");
 
 const randomNum = getRandomNumber();
 
@@ -20,6 +20,43 @@ function onSpeak(e) {
   checkNumber(msg);
 }
 
+// Write what user speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+        <div>You said:</div>
+        <span class="box">${msg}</span>
+    `;
+}
+
+// Check msg against number
+function checkNumber(msg) {
+  const num = +msg;
+
+  // Check if valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += "<div>That is not a valid number</div>";
+    return;
+  }
+
+  // Check in range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += "<div>Number must be between 1 and 100</div>";
+    return;
+  }
+
+  // Check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+        <h2>Congrats you have guessed the number! <br><br> It was ${num}</h2>
+        <button class="play-again" id="play-again">Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += "<div>Go lower</div>";
+  } else {
+    msgEl.innerHTML += "<div>Go higher</div>";
+  }
+}
+
 // Generate random number
 function getRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
@@ -27,3 +64,12 @@ function getRandomNumber() {
 
 // Speak result
 recognition.addEventListener("result", onSpeak);
+
+// End SR service
+recognition.addEventListener("end", () => recognition.start());
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.id == "play-again") {
+    window.location.reload();
+  }
+});
